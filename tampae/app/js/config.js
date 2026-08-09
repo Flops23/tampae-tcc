8525ee5d45403c86cd3c6167c3f537bbfdd80631
Flops23 <felipealvesdeazevedo2007@gmail.com>
2026-08-09 10:56:08 -0300

    fix: corrige chave publica do Supabase

--- tampae/app/js/config.js
+++ tampae/app/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     SUPABASE_URL: "https://usztjfxtbagjbnupiuxm.supabase.co",
-    SUPABASE_KEY: "sb_publishable_SZhZ1FlWxXzd85fWRQ69Fg_j9HXqqnr",
+    SUPABASE_ANON_KEY: "sb_publishable_SZhZ1FlWxXzd85fWRQ69Fg_j9HXqqnr",
 
     APP_NAME: "TampAÊ",
     APP_VERSION: "1.0.0",
